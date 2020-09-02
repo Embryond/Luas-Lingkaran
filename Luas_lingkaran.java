552d5ebd2df554a97f8766decdd2c74450db1ec9
Embryond <69889336+Embryond@users.noreply.github.com>
2020-09-02 13:23:46 +0700

    Add files via upload

---
+++ Luas_lingkaran.java
@@ -0,0 +1,15 @@
+
+package luas.lingkaran;
+
+
+public class Luas_lingkaran {
+    public static void main(String[] args){
+        double jarijari = 14;
+        double phi = 3.14;
+        double luasLingkaran = phi*jarijari*jarijari;
+        System.out.println("jarijari = "+jarijari);
+        System.out.println("phi = "+phi);
+        System.out.println("Luas Lingkaran Adalah = "+luasLingkaran);
+    }
+    
+}
